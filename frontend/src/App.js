ef846a7deb8e9308703f1b50f8f088fb183d7d70
heojae <shrldh3576@naver.com>
2021-02-15 21:57:25 +0900

    Set base pages login and tool 로그인 과 툴 페이지에 대해서, 기초를 만들었다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React, {Component} from "react";
+import {Link, Route, BrowserRouter as Router} from "react-router-dom"
 import './App.css';
+
+import Login from "./pages/Login"
+import Tool from "./pages/Tool"
 
 
 class App extends Component {
     render() {
-        return (<div>AAAAa</div>)
+        return (
+            <Router>
+                <div className={"App"}>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/" component={Tool}/>
+                </div>
+            </Router>
+        )
     }
 }
 
