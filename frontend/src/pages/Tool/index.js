75728339a70c6fc2a6412d34339de9e1285b8436
heojae <shrldh3576@naver.com>
2021-02-17 22:24:45 +0900

    Add feature to check cookie and whether to redirect or not 쿠키를 확인하고, /login 으로 리다이렉트 할지 안할지에 대한 구현을 마쳤다.

--- frontend/src/pages/Tool/index.js
+++ frontend/src/pages/Tool/index.js
@@ -1,10 +1,49 @@
 import React, {Component} from "react"
-
+import Cookies from "universal-cookie";
+import {withRouter} from 'react-router-dom';
+import {getUserInfoWithToken} from "./API";
+import * as actions from "../../actions";
+import {connect} from "react-redux";
 
 class Tool extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        const cookies = new Cookies();
+        const access_token = cookies.get("access_token");
+        if (access_token) {
+            const metadata = {access_token};
+            const response = await getUserInfoWithToken(metadata);
+            if (response) {
+                const [pk, email, profile_image, access_token] = response;
+                this.props.handleSetUserInfo(pk, email, profile_image, access_token);
+            } else {
+                this.props.history.push("/login")
+            }
+        } else {
+            this.props.history.push("/login")
+        }
+    }
+
     render() {
         return (<div> Tool </div>)
     }
 }
 
-export default Tool;
+
+const mapStateToProps = (state) => {
+    return {};
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleSetUserInfo: (pk, email, profile_image, access_token) => {
+            dispatch(actions.setUserInfo(pk, email, profile_image, access_token))
+        }
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Tool));
+
+
