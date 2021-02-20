380438b094f0737729cf80e71e4a41fd8256b812
heojae <shrldh3576@naver.com>
2021-02-20 09:02:01 +0900

    Add feature ToolSidebar show or not with redux 사이드바의 클릭에 대해서, show or not 을 할 수 있는 기능을 구현하였다.

--- frontend/src/pages/Tool/ToolMiddle/ToolSidebar/index.js
+++ frontend/src/pages/Tool/ToolMiddle/ToolSidebar/index.js
@@ -1,14 +1,37 @@
 import React, {Component} from "react"
+import * as actions from "../../../../actions";
+import {connect} from "react-redux";
 
 
 class ToolSidebar extends Component {
-    render() {
-        return (<div> ToolSidebar </div>)
+    constructor(props) {
+        super(props);
     }
 
+    render() {
+        return (
+            <div className="Tool-sidebar" style={{display: this.props.sidebar_show_or_not ? "block" : "none"}}>
+                ToolSidebar
+            </div>
+        )
+    }
 }
 
-export default ToolSidebar;
+const mapStateToProps = (state) => {
+    return {
+        sidebar_show_or_not: state.sidebar.show_or_not
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleSetToolMode: (mode) => {
+            dispatch(actions.setToolMode(mode))
+        }
+    };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToolSidebar);
 
 
 
--- frontend/src/pages/Tool/ToolMiddle/index.js
+++ frontend/src/pages/Tool/ToolMiddle/index.js
@@ -1,21 +1,44 @@
-import React, {Component} from "react"
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import * as actions from "../../../actions";
 import "./index.css"
 
 import ToolSidebar from "./ToolSidebar";
 
 class ToolMiddle extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
+        const display_none_style = {
+            display: "none"
+        }
+
         return (
             <div className={"Tool-middle"}>
-                <ToolSidebar/>
+                <ToolSidebar />
                 <div>Tool Main</div>
             </div>
         )
     }
-
 }
 
-export default ToolMiddle;
+
+const mapStateToProps = (state) => {
+    return {
+        sidebar_show_or_not: state.sidebar.show_or_not
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleSetSidebarShowOrNot: () => {
+            dispatch(actions.setSidebarShowOrNot())
+        }
+    };
+}
 
 
+export default connect(mapStateToProps, mapDispatchToProps)(ToolMiddle);
 
