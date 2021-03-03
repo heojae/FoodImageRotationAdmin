8304b2fd539dae7f551d8fdc521ad2b4961119f3
heojae <shrldh3576@naver.com>
2021-03-04 01:27:02 +0900

    Add dataset server nginx to docker-compose.yml 1. datset sercer nginx dockerfile 추가 2. docker-compose.yml 에 추가 3. media server 등록 완료

---
+++ backend/mysql-dataset/sqls/initialize.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS fira_dataset;
+
+
+CREATE DATABASE fira_dataset;
+USE fira_dataset;
+
+CREATE TABLE dataset_info
+(
+    pk    INTEGER AUTO_INCREMENT,
+    title VARCHAR(100),
+    PRIMARY KEY (pk)
+);
+
+CREATE TABLE image_info
+(
+    pk                 INTEGER AUTO_INCREMENT,
+    dataset_info_pk    INTEGER,
+    original_file_name VARCHAR(40),
+    uuid_file_name     VARCHAR(40),
+    model_degree       INTEGER,
+    confidence         DOUBLE,
+    user_fix_degree    INTEGER,
+    PRIMARY KEY (pk)
+);
+
+
+
+INSERT INTO dataset_info(title)
+VALUES ('untrained');
+
